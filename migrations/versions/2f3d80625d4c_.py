a0b85f76f06614659fd36fb60e87e66702fedf1b
hudi <h_udi@qq.com>
2018-05-21 20:47:39 +0800

    add db doc

---
+++ migrations/versions/2f3d80625d4c_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 2f3d80625d4c
+Revises: 
+Create Date: 2018-05-21 20:46:22.807007
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '2f3d80625d4c'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
